4e6f9f6efb6759d80c142488dc90d4cc5ee10dfa
Albert Lyu <albertworld@gmail.com>
2014-05-06 18:59:26 -0500

    Added idea_app

---
+++ idea_app/app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  def new
+  end
+  
+  def create
+    idea = Idea.new
+    idea.description = params['description']
+    idea.save
+    redirect_to "/ideas/#{ idea.id }"
+  end
+  
+  def show
+    @idea = Idea.find_by_id(params['id'])
+  end
+  
+  def edit
+    @idea = Idea.find_by_id(params['id'])
+  end
+  
+  def update
+    idea = Idea.find_by_id(params['id'])
+    idea.description = params['description']
+    idea.save
+    redirect_to "/ideas/#{ idea.id }"
+  end
+end
---
+++ idea_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdeaApp::Application.config.secret_key_base = 'e3d2a5e9c7c6408585367d9f19f09bab59b6dec38e915a1216dc992f8a1f22c4246e1152939bff16d8966ee3cec4cbba9dfbbe046ac8a4e48de2588f179a3f1c'
